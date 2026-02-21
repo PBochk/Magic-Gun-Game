bb69cbd6f48898480b5730e3532a3c852cdd22b9
v_yashchik <medyantsevsvyt@gmail.com>
2026-02-22 00:06:31 +0500

    feat: test enemy stab implemented

--- Assets/Scripts/Battle/Enemy/EnemyBodyPart.cs
+++ Assets/Scripts/Battle/Enemy/EnemyBodyPart.cs
@@ -1,4 +1,12 @@
 ﻿public abstract class EnemyBodyPart : BattleEntity
 {
+    public override DamageTakenEventArgs TakeDamage(DamageInfo damageInfo)
+    {
+        throw new System.NotImplementedException();
+    }
 
+    public override EffectAppliedEventArgs ApplyEffect()
+    {
+        throw new System.NotImplementedException();
+    }
 }
--- Assets/Scripts/Battle/Enemy/EnemyController.cs
+++ Assets/Scripts/Battle/Enemy/EnemyController.cs
@@ -1,7 +1,52 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
-using Zenject;
 
 public abstract class EnemyController : MonoBehaviour
 {
+    public IReadOnlyList<EnemyBodyPart> BodyParts => bodyParts;
 
+    [SerializeField] private EnemyBodyPart[] bodyParts;
+
+    #region Validation
+    public void OnValidate()
+    {
+        if (bodyParts == null || bodyParts.Length == 0) return;
+        if (!RemoveDuplicates()) return;
+
+        RemoveNullEntries();
+    }
+
+    private bool RemoveDuplicates()
+    {
+        HashSet<EnemyBodyPart> uniqueParts = new();
+        return MarkDuplicateEntries(uniqueParts);
+    }
+
+    private bool MarkDuplicateEntries(HashSet<EnemyBodyPart> uniqueParts)
+    {
+        var duplicatesFound = false;
+
+        for (var i = 0; i < bodyParts.Length; i++)
+        {
+            EnemyBodyPart part = bodyParts[i];
+
+            if (part == null || uniqueParts.Add(part)) continue;
+
+            LogDuplicateBodyPart(part);
+            bodyParts[i] = null;
+            duplicatesFound = true;
+        }
+
+        return duplicatesFound;
+    }
+
+    private void RemoveNullEntries() =>
+        bodyParts = bodyParts.Where(p => p != null).ToArray();
+
+    private void LogDuplicateBodyPart(EnemyBodyPart part) =>
+        Debug.LogWarning(
+            $"Duplicate body part '{part.name}' was removed from the list on object {gameObject.name}",
+            gameObject);
+    #endregion
 }
