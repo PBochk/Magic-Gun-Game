a8c6dc6b159ee31708e62dc2687b47109c69238e
v_yashchik <medyantsevsvyt@gmail.com>
2026-02-22 16:08:53 +0500

    style: DamageTakenEventArgs class corrected

--- Assets/Scripts/Battle/DamageTakenEventArgs.cs
+++ Assets/Scripts/Battle/DamageTakenEventArgs.cs
@@ -2,11 +2,27 @@ using System;
 
 public sealed class DamageTakenEventArgs : EventArgs
  {
-    public DamageInfo DamageInfo { get; }
-    public int DamageReceived { get; }
-    public int HealthDamageReceived { get; }
-    public bool IsBodyPartElementBreak { get; }
-    public bool IsBodyPartDefeated { get; }
-    public bool WasKilled { get; }
-        //...etc, can be extended
+     public DamageInfo DamageInfo { get; }
+     public int DamageToHealthReceived { get; }
+     public int DamageToStaminaReceived { get; }
+     public bool IsBodyPartElementBreak { get; }
+     public bool IsBodyPartDefeated { get; }
+     public bool WasDefeated { get; }
+     //...etc, can be extended
+     
+     public DamageTakenEventArgs(
+         DamageInfo damageInfo,
+         int damageToHealthReceived,
+         int damageToStaminaReceived,
+         bool isBodyPartElementBreak,
+         bool isBodyPartDefeated,
+         bool wasDefeated)
+     {
+         DamageInfo = damageInfo;
+         DamageToHealthReceived = damageToHealthReceived;
+         DamageToStaminaReceived = damageToStaminaReceived;
+         IsBodyPartElementBreak = isBodyPartElementBreak;
+         IsBodyPartDefeated = isBodyPartDefeated;
+         WasDefeated = wasDefeated;
+     }
  }
